4387b9d6f44b64d3971f2a6a495141855a336ffc
MarceloCPadilha <marcelocpadilha51@gmail.com>
2026-09-17 11:55:39 -0300

    .

--- Semestre_2/programacao_orientada_a_objetos/0917/02.py
+++ Semestre_2/programacao_orientada_a_objetos/0917/02.py
@@ -5,12 +5,21 @@ class Televisor:
         self.volume = 0 # int
         self.estado = 0 # bool: Desligado/Ligado; 0/1
 
+    def exibir_informacoes(self):
+        for arg in dir(self):
+            # Como o loop em cima vai pegar muito mais do que só as variaveis e os valores a gente filtra
+            if not arg.startswith('__') and not callable(getattr(self, arg := arg)):
+                valor = getattr(self, arg)
+                print("{}: {}".format(arg, valor))
 
     def botãoPower(self):
         if self.estado is True:
             self.estado = False
         elif self.estado is False:
             self.estado = True
+
+    def definir_volume(self, volume):
+        self.volume = volume
 
     def aumentar_volume(self):
         if self.volume == 100:
@@ -24,16 +33,23 @@ class Televisor:
         else:
             volume -= 1
 
-    def trocar_canal(self, canal):
-        if canal == "+":
-            self.canal = (canal + 1) % 101
-        elif canal == "-":
-            self.canal = (canal - 1) * 101
-        elif isinstance(canal, int) and 0 <= canal < 101:
-            self.canal = canal
+    def trocar_canal(self, operando):
+        if operando == "+":
+            self.canal = (self.canal + 1) % 101
+        elif operando == "-":
+            self.canal = (self.canal - 1) % 101
+        elif isinstance(self.canal, int) and 0 <= canal < 101:
+            self.canal = operando
         else:
             print("Canal Inválido")
 
 
 
 tv1 = Televisor("Sansumg Smart TV")
+tv1.definir_volume(100)
+tv1.exibir_informacoes()
+print()
+tv1.aumentar_volume()
+tv1.trocar_canal("-")
+tv1.exibir_informacoes()
+print()
